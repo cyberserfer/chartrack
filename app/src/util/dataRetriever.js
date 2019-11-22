5b683551b3ed1500774f897e9b986f1f4f957253
Cary Wolff <carywolff@northwesternmutual.com>
2019-11-21 19:50:27 -0600

    feat: added eslint

--- app/src/util/dataRetriever.js
+++ app/src/util/dataRetriever.js
@@ -1,18 +1,17 @@
 // Async/Await approach https://tutorialzine.com/2017/07/javascript-async-await-explained
 
 // The async keyword will automatically create a new Promise and return it.
-async function getJSONAsync() {
+async function getJSONAsync () {
+  // The await keyword saves us from having to write a .then() block.
+  const json = await axios.get('https://tutorialzine.com/misc/files/example.json')
 
-    // The await keyword saves us from having to write a .then() block.
-    let json = await axios.get('https://tutorialzine.com/misc/files/example.json');
-
-    // The result of the GET request is available in the json variable.
-    // We return it just like in a regular synchronous function.
-    return json;
+  // The result of the GET request is available in the json variable.
+  // We return it just like in a regular synchronous function.
+  return json
 }
 
-//We can call our async function like this:
+// We can call our async function like this:
 
 getJSONAsync().then(function (result) {
-    // Do something with result.
-});
+  // Do something with result.
+})
