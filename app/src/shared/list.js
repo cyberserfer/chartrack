d8725a473cff5efb93c4258762c516e35e56eef3
Paul Buss <cyberserfer@gmail.com>
2019-11-24 19:13:21 -0600

    chore: refactored components to use helper"

---
+++ app/src/shared/list.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+
+const List = ({objName, objValue, possibleValues, funcOne, funcTwo }) => {
+	console.log('objName');
+	console.log(objName);
+		
+        return (
+			<div key={objName}>
+            <Grid container spacing={3}>
+              <Grid item xs={4}>
+                {objName}
+              </Grid>
+              <Grid item xs={2}>
+                <span />
+              </Grid>
+              <Grid item xs={6}>
+			<select
+			name={objName}
+			onChange={e => funcOne(e)}
+			defaultValue={objValue}
+		  >
+			{possibleValues.map((dieType, i) =>
+			  <option
+				key={i}
+				value={dieType}
+			  >
+				{dieType}
+			  </option>)}
+		  </select>
+		  </Grid>
+            </Grid>
+          </div>
+		)
+	
+
+}
+
+export default List
