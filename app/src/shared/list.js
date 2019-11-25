1ea9355be4cd136ec11ed14166757aa3793cf852
Paul Buss <cyberserfer@gmail.com>
2019-11-24 22:18:53 -0600

    fix: fixed list filename

--- app/src/shared/list.js
+++ app/src/shared/list.js
@@ -4,29 +4,29 @@ import Grid from '@material-ui/core/Grid'
 const List = ({objName, objValue, possibleValues, funcOne }) => {
 		
         return (
-			<div key={objName}>
-            <Grid container spacing={3}>
-              <Grid item xs={4}>
-                {objName}
-              </Grid>
-              <Grid item xs={2}>
-                <span />
-              </Grid>
-              <Grid item xs={6}>
-			<select
-				name={objName}
-				onChange={e => funcOne(e)}
-				defaultValue={objValue}
-		  	>
-			{possibleValues.map((dieType, i) =>
-			  <option
-				key={i}
-				value={dieType}
-			  >
-				{dieType}
-			  </option>)}
-		  </select>
-		  </Grid>
+				<div key={objName}>
+				<Grid container spacing={3}>
+					<Grid item xs={4}>
+						{objName}
+					</Grid>
+					<Grid item xs={2}>
+						<span />
+					</Grid>
+					<Grid item xs={6}>
+					<select
+						name={objName}
+						onChange={e => funcOne(e)}
+						defaultValue={objValue}
+					>
+					{possibleValues.map((dieType, i) =>
+					<option
+						key={i}
+						value={dieType}
+					>
+						{dieType}
+					</option>)}
+					</select>
+				</Grid>
             </Grid>
           </div>
 		)
