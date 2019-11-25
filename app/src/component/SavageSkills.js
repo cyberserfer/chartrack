65f408a32c64142a7abd3a055ccdf5c27727d5f5
Paul Buss <cyberserfer@gmail.com>
2019-11-24 19:13:21 -0600

    chore: refactored components to use helper"

--- app/src/component/SavageSkills.js
+++ app/src/component/SavageSkills.js
@@ -1,41 +1,21 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
+import List from '../shared/List'
 
 const SavageSkills = ({ updateSkills, skills, possibleValues }) => (
   <div>
     <h2>Skills</h2>
     <div>
       {Object.entries(skills).map(skill => {
-        const skillName = skill[0]
-        const skillvalue = skill[1]
-
-        return (
-          <div key={skillName}>
-            <Grid container spacing={3}>
-              <Grid item xs={4}>
-                {skillName}
-              </Grid>
-              <Grid item xs={2}>
-                <span />
-              </Grid>
-              <Grid item xs={6}>
-                <select
-                  name={skillName}
-                  onChange={e => updateSkills(e)}
-                  defaultValue={skillvalue}
-                >
-                  {possibleValues.map((dieType, i) =>
-                    <option
-                      key={i}
-                      value={dieType}
-                    >
-                      {dieType}
-                    </option>)}
-                </select>
-              </Grid>
-            </Grid>
-          </div>
-        )
+        const objName = skill[0]
+          const objValue = skill[1]
+          return (
+            <List 
+              objName={objName}
+              objValue={objValue}
+              possibleValues={possibleValues} 
+              funcOne={updateSkills} 
+            />
+          )
       })}
     </div>
   </div>
