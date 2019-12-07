65a74f968ad8aa938296be20935bd20ed57dfe02
Cary Wolff <carywolff@northwesternmutual.com>
2019-12-07 13:42:43 -0600

    added login path to chartsheet

--- app/src/App.js
+++ app/src/App.js
@@ -9,10 +9,17 @@ import './App.css';
 import AppHeader from './container/AppHeader';
 import SavageSheet from './container/SavageSheet';
 import LandingContainer from './container/LandingContainer';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+});
 
 export default class App extends Component {
   render() {
     return (
+      <ApolloProvider client={client}>      
       <Router>
         <div>
           <header className="App-header">
@@ -25,6 +32,7 @@ export default class App extends Component {
           </Switch>
         </div>
       </Router>
+      </ApolloProvider>
     );
   }
 }
