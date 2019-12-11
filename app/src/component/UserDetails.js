d9db7468ec8178c0cbb5b499551f95e5ed524d09
Paul Buss <cyberserfer@gmail.com>
2019-12-10 19:44:19 -0600

    added fader to user details click

--- app/src/component/UserDetails.js
+++ app/src/component/UserDetails.js
@@ -1,7 +1,29 @@
 import React, {useState} from 'react';
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
+const fadeIn = keyframes`
+  from {
+    transform: scale(.25);
+    opacity: 0;
+  }
 
+  to {
+    transform: scale(1);
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    transform: scale(1);
+    opacity: 0;
+  }
+
+  to {
+    transform: scale(.25);
+    opacity: 1;
+  }
+`;
 
 const Avatar = styled.div`
     position: absolute;
@@ -24,6 +46,9 @@ const AvatarDetails = styled.div`
     border-width: 2px;
     border-style: solid;
     background: lightgrey;
+    visibility: ${props => props.out ? 'hidden' : 'visible'};
+    animation: ${props => props.out ? fadeOut : fadeIn} 1s linear;
+    transition: visibility 1s linear;
 `
 
 function UserDetails() {
